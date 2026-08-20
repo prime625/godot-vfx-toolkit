cb8c2227ef74c552cc6e037a47ad9294140c6489
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-08-20 22:00:17 +0200

    Update register_types.cpp

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -12,6 +12,7 @@
 #include "vfx_texture_painter.h"
 #include "vfx_scene.h"
 #include "vfx_scene_node.h"
+#include "vfx_scene_tree_panel.h"
 #include "vfx_uv_editor_viewport.h"
 #include "vfx_particles.h"
 
@@ -29,6 +30,7 @@ void initialize_vfx_toolkit(ModuleInitializationLevel p_level) {
 		ClassDB::register_class<VFXTexturePainter>();
 		ClassDB::register_class<VFXScene>();
 		ClassDB::register_class<VFXSceneNode>();
+		ClassDB::register_class<VFXSceneTreePanel>();
 		ClassDB::register_class<VFXUVEditorViewport>();
 		ClassDB::register_class<VFXParticles3D>();
 	}
@@ -40,9 +42,9 @@ void uninitialize_vfx_toolkit(ModuleInitializationLevel p_level) {
 
 extern "C" {
 GDExtensionBool GDE_EXPORT vfx_toolkit_library_init(
-	GDExtensionInterfaceGetProcAddress p_get_proc_address,
-	GDExtensionClassLibraryPtr p_library,
-	GDExtensionInitialization *r_initialization) {
+		GDExtensionInterfaceGetProcAddress p_get_proc_address,
+		GDExtensionClassLibraryPtr p_library,
+		GDExtensionInitialization *r_initialization) {
 
 	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 	init_obj.register_initializer(initialize_vfx_toolkit);
