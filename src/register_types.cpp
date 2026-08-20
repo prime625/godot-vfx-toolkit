d21ac9d503aa6a5651c691943c8afabab80e6119
sheltonndlovu625-sketch <sheltonndlovu625@gmail.com>
2026-08-20 14:26:04 +0200

    Update register_types.cpp

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -13,40 +13,41 @@
 #include "vfx_scene.h"
 #include "vfx_scene_node.h"
 #include "vfx_uv_editor_viewport.h"
+#include "vfx_particles.h"
 
 void initialize_vfx_toolkit(ModuleInitializationLevel p_level) {
-    if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
-        ClassDB::register_class<VFXEditorNode>();
-        ClassDB::register_class<VFXMesh>();
-        ClassDB::register_class<VFXSkeleton>();
-        ClassDB::register_class<VFXSkin>();
-        ClassDB::register_class<VFXAnimator>();
-        ClassDB::register_class<VFXGLTFExporter>();
-        ClassDB::register_class<VFXCurve>();
-        ClassDB::register_class<VFXUVEditor>();
-        ClassDB::register_class<VFXRetopology>();
-        ClassDB::register_class<VFXTexturePainter>();
-        ClassDB::register_class<VFXScene>();
-		ClassDB::register_class<VFXUVEditorViewport>();
+	if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+		ClassDB::register_class<VFXEditorNode>();
+		ClassDB::register_class<VFXMesh>();
+		ClassDB::register_class<VFXSkeleton>();
+		ClassDB::register_class<VFXSkin>();
+		ClassDB::register_class<VFXAnimator>();
+		ClassDB::register_class<VFXGLTFExporter>();
+		ClassDB::register_class<VFXCurve>();
+		ClassDB::register_class<VFXUVEditor>();
+		ClassDB::register_class<VFXRetopology>();
+		ClassDB::register_class<VFXTexturePainter>();
+		ClassDB::register_class<VFXScene>();
 		ClassDB::register_class<VFXSceneNode>();
-
-    }
+		ClassDB::register_class<VFXUVEditorViewport>();
+		ClassDB::register_class<VFXParticles3D>();
+	}
 }
 
 void uninitialize_vfx_toolkit(ModuleInitializationLevel p_level) {
-    // Cleanup if needed
+	// Cleanup if needed
 }
 
 extern "C" {
 GDExtensionBool GDE_EXPORT vfx_toolkit_library_init(
-    GDExtensionInterfaceGetProcAddress p_get_proc_address,
-    GDExtensionClassLibraryPtr p_library,
-    GDExtensionInitialization *r_initialization) {
+	GDExtensionInterfaceGetProcAddress p_get_proc_address,
+	GDExtensionClassLibraryPtr p_library,
+	GDExtensionInitialization *r_initialization) {
 
-    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-    init_obj.register_initializer(initialize_vfx_toolkit);
-    init_obj.register_terminator(uninitialize_vfx_toolkit);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-    return init_obj.init();
+	godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+	init_obj.register_initializer(initialize_vfx_toolkit);
+	init_obj.register_terminator(uninitialize_vfx_toolkit);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+	return init_obj.init();
 }
 }
